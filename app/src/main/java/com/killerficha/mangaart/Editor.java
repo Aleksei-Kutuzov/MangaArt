6984c339c8f20deefed6a51cae6950482c7343c3
Aleksei-Kutuzov <alekseikutuzov2@yandex.com>
2025-02-15 13:41:21 +0300

    1

--- app/src/main/java/com/killerficha/mangaart/Editor.java
+++ app/src/main/java/com/killerficha/mangaart/Editor.java
@@ -1,10 +1,6 @@
 package com.killerficha.mangaart;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    ImageButton editorButton;
-
+public class Editor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_editor);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        editorButton = findViewById(R.id.getToEditir);
-        editorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this, Editor.class);
-                startActivity(intent);
-            }
         });
     }
 }
