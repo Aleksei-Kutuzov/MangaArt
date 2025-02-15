f14d977be95e4aec4a00271eca26136e4bbc0813
Aleksei-Kutuzov <alekseikutuzov2@yandex.com>
2025-02-15 14:03:06 +0300

    2

--- app/src/main/java/com/killerficha/mangaart/Editor.java
+++ app/src/main/java/com/killerficha/mangaart/Editor.java
@@ -1,6 +1,7 @@
 package com.killerficha.mangaart;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Editor extends AppCompatActivity {
+
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,7 @@ public class Editor extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        view = findViewById(R.id.view);
+        setContentView(new EditorDraw(this), view);
     }
 }
---
+++ app/src/main/java/com/killerficha/mangaart/EditorDraw.java
@@ -0,0 +1,16 @@
+package com.killerficha.mangaart;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public class EditorDraw extends View {
+    public EditorDraw (Context context) {
+        super(context);
+    }
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
