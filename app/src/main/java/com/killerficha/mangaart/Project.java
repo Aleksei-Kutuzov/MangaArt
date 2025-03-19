8a234d4601a693aee71ae9255a8524b7f120a3ca
Xakepl <lucoroalava@gmail.com>
2025-03-19 18:14:50 +0300

    2222222

--- app/src/main/java/com/killerficha/mangaart/Project.java
+++ app/src/main/java/com/killerficha/mangaart/Project.java
@@ -1,14 +1,48 @@
 package com.killerficha.mangaart;
 
+import static android.opengl.ETC1.getHeight;
+import static android.opengl.ETC1.getWidth;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.Log;
 
 import java.io.Serializable;
+import java.util.AbstractCollection;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Project implements Serializable {
     private List<PageHistory> pages;
+    private List<DrawableObject> freeLines;
+
     private int enabledPageIndex;
+    Canvas canvas;
+
+    enum templ {EMPTY_T, TWO_X_TWO, THREE_X_TWO, FOUR_X_TWO, CUSTOM}
+
+    templ modeT = templ.TWO_X_TWO;
+    private int rows = 2; // Количество строк
+    private int cols = 2; // Количество столбцов
+    int cellHeight;
+    int cellWidth;
+    private Paint paint;
+
+    int getRows() {
+        return rows;
+    }
+
+    void setRows(int r) {
+        rows = r;
+    }
+
+    int getCols() {
+        return cols;
+    }
+
+    void setCols(int c) {
+        cols = c;
+    }
 
     Project(){
         pages = new ArrayList<>();
@@ -21,8 +55,58 @@ public class Project implements Serializable {
         enabledPageIndex = pages.size() - 1;
     }
 
+    public void pageAdd(){pageAdd(new PageHistory());}
+
     public void pageAdd(){
-        pageAdd(new PageHistory());
+//pageAdd();
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        switch (modeT) {
+            case EMPTY_T:
+                break;
+            case TWO_X_TWO:
+                // Рассчитываем ширину и высоту ячейки
+                setRows(2);
+                setCols(2);
+                cellWidth = width / getCols();
+                cellHeight = height / getRows();
+
+                break;
+            case THREE_X_TWO:
+                // Рассчитываем ширину и высоту ячейк
+                setRows(3);
+                setCols(2);
+                cellWidth = width / getCols();
+                cellHeight = height / getRows();
+                break;
+            case FOUR_X_TWO:
+                // Рассчитываем ширину и высоту ячейки
+                setRows(4);
+                setCols(2);
+                cellWidth = width / getCols();
+                cellHeight = height / getRows();
+                break;
+            //case CUSTOM:
+//                setCols();
+        }
+        pageAdd();
+        for (int i = 1; i < cols; i++) {
+            int x = i * cellWidth;
+//            canvas.drawLine(0, 0, x, height, paint);
+            DrawableObject newDrawable = new FreeLine(x, 0);
+            newDrawable.setPaint(new Paint(paint));
+            freeLines.add(newDrawable);
+
+        }
+        // Рисуем горизонтальные линии
+        for (int i = 1; i < rows; i++) {
+            int y = i * cellHeight;
+//            canvas.drawLine(0, y, width, y, paint);
+            DrawableObject newDrawable = new FreeLine(0, y);
+            newDrawable.setPaint(new Paint(paint));
+            freeLines.add(newDrawable);
+        }
     }
 
     public int getEnabledPageIndex() {
